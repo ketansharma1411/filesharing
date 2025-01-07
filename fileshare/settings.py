010988c07858a6f66dfdf73eef07d2ff46e8e800
ketansharma1411 <ketansharma1411@gmail.com>
2025-01-07 15:09:11 +0530

    changes in settings .py adding witenoise middleware

--- fileshare/settings.py
+++ fileshare/settings.py
@@ -46,7 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'whitenoise.runserver_nostatic'
+    'whitenoise.runserver_nostatic',
     'files',
 ]
 
