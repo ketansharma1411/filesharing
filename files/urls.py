f895fb80367c38d87d5b6428c76cb9dfc23299df
ketansharma1411 <ketansharma1411@gmail.com>
2025-01-14 21:28:20 +0530

    sitemap added, robots.txt file added

--- files/urls.py
+++ files/urls.py
@@ -4,8 +4,8 @@ from . import views
 urlpatterns = [
     path('', views.upload_file, name='upload_file'),
     # path('download/', views.download_file, name='download_file'),
-    path('about/',views.about,name='about'),
-    path('display/', views.display_files, name='display_files'),  # Display all files
+    path('about',views.about,name='about'),
+    path('display', views.display_files, name='display_files'),  # Display all files
     path('download/<uuid:file_id>/<int:ID>', views.download_file, name='download_file'),  # Download specific file
     path('upload_success/<uuid:file_id>', views.upload_success, name='upload_success'),
     path('testing',views.testing,name='testing')
